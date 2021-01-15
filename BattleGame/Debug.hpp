f3b8c1c1417359f33a5c9344dfa7c2901640780d
RyumaNoma <rskm.2002722@gmail.com>
2021-01-15 20:38:00 +0900

    debug用ファイル出力関数の引数を可変引数にした

--- BattleGame/Debug.hpp
+++ BattleGame/Debug.hpp
@@ -1,9 +1,40 @@
 #pragma once
 
-#include <string>
 #include <fstream>
+#include <utility>
+#include <cstdio>
 
+#ifdef _DEBUG
 namespace game
 {
-	void Debug(std::string str);
+	void debugReset(void)
+	{
+		FILE* out;
+		fopen_s(&out, "./data/Debug.txt", "w");
+		fclose(out);
+	}
+
+	template <class... Args>
+	void debug(const char* format, const Args& ...args)
+	{
+		FILE* out;
+		fopen_s(&out, "./data/Debug.txt", "a");
+		if (out)
+		{
+			fprintf(out, format, args...);
+			fclose(out);
+		}
+	}
 }
+#else
+namespace game
+{
+	void debugReset(void)
+	{
+	}
+	template <class... Args>
+	void debug(const char* format, const Args& ...args)
+	{
+	}
+}
+#endif
--- BattleGame/Header.hpp
+++ BattleGame/Header.hpp
@@ -1,7 +1,3 @@
 #pragma once
 
-#include <string>
-#include <iostream>
-#include <fstream>
-
 #include "Debug.hpp"
