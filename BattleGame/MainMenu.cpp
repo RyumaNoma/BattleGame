b79f1e886f0eb540b33eee16498c37b320164aee
RyumaNoma <rskm.2002722@gmail.com>
2021-01-19 16:02:46 +0900

    MainMenuシーンを追加

---
+++ BattleGame/MainMenu.cpp
@@ -0,0 +1,14 @@
+#include "MainMenu.hpp"
+
+namespace game
+{
+	MainMenu::MainMenu(const InitData& init) : IScene(init)
+	{
+	}
+	void MainMenu::update()
+	{
+	}
+	void MainMenu::draw() const
+	{
+	}
+}
---
+++ BattleGame/MainMenu.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Siv3D.hpp>
+#include "SceneData.hpp"
+
+namespace game
+{
+	using App = SceneManager<int, SceneData>;
+
+	class MainMenu : App::Scene
+	{
+	public:
+		MainMenu(const InitData& init);
+		void update() override;
+		void draw() const override;
+	};
+}
+
