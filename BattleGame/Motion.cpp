9b09dab86deaf71d6e16bf337b57e58d940e24db
RyumaNoma <rskm.2002722@gmail.com>
2021-01-20 17:31:57 +0900

    rotateSordを追加

--- BattleGame/Motion.cpp
+++ BattleGame/Motion.cpp
@@ -6,13 +6,24 @@ namespace game
 	{
 		void jump(Fighter& fighter, int& fighterX, int& fighterY)
 		{
-			int lastPos = -sin(fighter.getFlame() / 50.0 * Math::Pi) * 100;
-			fighter.incFlame();
-			int nowPos = -sin(fighter.getFlame() / 50.0 * Math::Pi) * 100;
+			int lastPos = -sin(fighter.getInAirTime() / 50.0 * Math::Pi) * 100;
+			fighter.incInAirTime();
+			int nowPos = -sin(fighter.getInAirTime() / 50.0 * Math::Pi) * 100;
 			
 			fighterY += nowPos - lastPos;
 
-			if (fighter.getFlame() == 50)
+			if (fighter.getInAirTime() == 50)
+			{
+				fighter.resetInAirTime();
+			}
+		}
+
+		void rotateSord(Fighter& fighter, int& fighterX, int& fighterY)
+		{
+			fighter.incFlame();
+			fighter.addRotate(6, 18);
+
+			if (fighter.getFlame() == 20)
 			{
 				fighter.resetFlame();
 				fighter.resetMotionNum();
--- BattleGame/Motion.hpp
+++ BattleGame/Motion.hpp
@@ -8,5 +8,6 @@ namespace game
 	namespace motion
 	{
 		void jump(Fighter& fighter, int& fighterX, int& fighterY);
+		void rotateSord(Fighter& fighter, int& fighterX, int& fighterY);
 	}
 }
