d1363cd77954fd7e391a3c59674b02866d4c37ab
RyumaNoma <rskm.2002722@gmail.com>
2021-01-21 07:45:12 +0900

    motionTableの追加

--- BattleGame/Motion.cpp
+++ BattleGame/Motion.cpp
@@ -4,11 +4,17 @@ namespace game
 {
 	namespace motion
 	{
+		// å„çdíº, É_ÉÅÅ[ÉW
+		const int motionTable[2][2] = {
+			{0, 0},
+			{10, 10}// Palu
+		};
+
 		void jump(Fighter& fighter, int& fighterX, int& fighterY)
 		{
-			int lastPos = -sin(fighter.getInAirTime() / 50.0 * Math::Pi) * 100;
+			int lastPos = -sin(fighter.getInAirTime() / 50.0 * Math::Pi) * 200;
 			fighter.incInAirTime();
-			int nowPos = -sin(fighter.getInAirTime() / 50.0 * Math::Pi) * 100;
+			int nowPos = -sin(fighter.getInAirTime() / 50.0 * Math::Pi) * 200;
 			
 			fighterY += nowPos - lastPos;
 
@@ -22,12 +28,14 @@ namespace game
 		{
 			fighter.incFlame();
 			fighter.addRotate(6, 18);
+			fighter.setAttack(6, true);
 
 			if (fighter.getFlame() == 20)
 			{
+				fighter.setAttack(6, false);
 				fighter.resetFlame();
 				fighter.resetMotionNum();
-				fighter.setRigidityCount(60);
+				fighter.setRigidityCount(motionTable[1][0]);
 			}
 		}
 	}
--- BattleGame/Motion.hpp
+++ BattleGame/Motion.hpp
@@ -7,6 +7,9 @@ namespace game
 {
 	namespace motion
 	{
+		// å„çdíº, É_ÉÅÅ[ÉW
+		extern const int motionTable[2][2];
+
 		void jump(Fighter& fighter, int& fighterX, int& fighterY);
 		void rotateSord(Fighter& fighter, int& fighterX, int& fighterY);
 	}
