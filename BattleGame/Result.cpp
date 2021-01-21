71d2be39a083fe5b270c706e59684800c8adab7b
RyumaNoma <rskm.2002722@gmail.com>
2021-01-21 19:18:21 +0900

    リザルト画面を作った

--- BattleGame/Result.cpp
+++ BattleGame/Result.cpp
@@ -8,10 +8,16 @@ namespace game
 
 	void Result::update()
 	{
+		if (isPressedBackButton())
+		{
+			changeScene(0);
+		}
 	}
 
 	void Result::draw() const
 	{
-
+		drawBackButton();
+		FontAsset(U"Title")(U"{}P WIN"_fmt(getData().winner + 1)).drawAt(Scene::Center().x, 100, Palette::Black);
+		getData().fighter[getData().winner].draw(Scene::Center().x, 300);
 	}
 }
