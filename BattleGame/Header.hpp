322cf60a75f0ba7ffe5b895f963cd3415707a007
RyumaNoma <rskm.2002722@gmail.com>
2021-01-22 21:27:24 +0900

    ヘッダーファイル群の編集

--- BattleGame/Header.hpp
+++ BattleGame/Header.hpp
@@ -1,7 +1,23 @@
 #pragma once
 
+// 関数
 #include "Debug.hpp"
+#include "Button.hpp"
 #include "Fighter.hpp"
+
+// ゲームに関するヘッダーファイル
+#include "BodyPart.hpp"
 #include "Direction.hpp"
-#include "FighterEdit.hpp"
+#include "FighterState.hpp"
+#include "Fighter.hpp"
+#include "FighterData.hpp"
+#include "Motion.hpp"
+
+// シーン
+#include "SceneData.hpp"
+#include "MainMenu.hpp"
 #include "FighterEditMenu.hpp"
+#include "FighterEdit.hpp"
+#include "FighterSelect.hpp"
+#include "Battle.hpp"
+#include "Result.hpp"
