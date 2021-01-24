5c040c6a140944b2000d8e093c8375fb552db890
RyumaNoma <rskm.2002722@gmail.com>
2021-01-24 17:51:37 +0900

    FighterEditMenuシーンを画面の大きさに合わせた

--- BattleGame/FighterEditMenu.cpp
+++ BattleGame/FighterEditMenu.cpp
@@ -21,7 +21,7 @@ namespace game
 			changeScene(0);
 		}
 
-		if (SimpleGUI::Button(U"New Fighter", Vec2(1000, 50)))
+		if (SimpleGUI::Button(U"New Fighter", Vec2(1700, 50)))
 		{
 			getData().newFighter = true;
 			getData().fighterNum = -1;
@@ -32,7 +32,8 @@ namespace game
 			int cursorX = Cursor::Pos().x;
 			int cursorY = Cursor::Pos().y;
 
-			if (300 <= cursorX && cursorX <= 1000 && 185 <= cursorY && cursorY <= 185 + this->fighters.size() * 30)
+			if (Scene::Center().x - 700 <= cursorX && cursorX <= Scene::Center().x + 700 &&
+				185 <= cursorY && cursorY <= 185 + this->fighters.size() * 30)
 			{
 				int fighterNum = (cursorY - 185) / 30;
 				debug("fighterNum", fighterNum);
@@ -48,11 +49,11 @@ namespace game
 		drawBackButton();
 		FontAsset(U"Title")(U"Fighter Edit Menu").drawAt(Scene::Center().x, 50, Palette::Black);
 
-		Line(300, 200 - 15, 1000, 200 - 15).draw(Palette::Skyblue);
+		Line(Scene::Center().x - 700, 200 - 15, Scene::Center().x + 700, 200 - 15).draw(Palette::Skyblue);
 		for (int i = 0; i < fighters.size(); i++)
 		{
 			FontAsset(U"Normal")(this->fighters[i]).drawAt(Scene::Center().x, 200 + i * 30, Palette::Black);
-			Line(300, 200 + (i+1) * 30 - 15, 1000, 200 + (i+1) * 30 - 15).draw(Palette::Skyblue);
+			Line(Scene::Center().x - 700, 200 + (i+1) * 30 - 15, Scene::Center().x + 700, 200 + (i+1) * 30 - 15).draw(Palette::Skyblue);
 		}
 	}
 }
