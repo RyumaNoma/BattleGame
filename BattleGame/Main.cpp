9960883954e277dc4ede4e3daa9bb7c88bfd1e6f
RyumaNoma <rskm.2002722@gmail.com>
2021-01-24 16:43:33 +0900

    全シーンを取り込み使えるようになった

--- BattleGame/Main.cpp
+++ BattleGame/Main.cpp
@@ -6,11 +6,11 @@ using App = SceneManager<int, game::SceneData>;
 void Main()
 {
 	constexpr Size sceneSize = DisplayResolution::HD_1366x768;
-
+	// constexpr Size sceneSize = DisplayResolution::HD_1280x720;
 	Scene::Resize(sceneSize);
 	Window::Resize(sceneSize, WindowResizeOption::KeepSceneSize);
 	// 全画面化
-	// (void)Window::SetFullscreen(true, unspecified, WindowResizeOption::KeepSceneSize);
+	(void)Window::SetFullscreen(true, unspecified, WindowResizeOption::KeepSceneSize);
 
 	game::debugReset();
 	Scene::SetBackground(Palette::White);
@@ -18,9 +18,13 @@ void Main()
 	FontAsset::Register(U"Normal", 30);
 
 	App manager;
-
+	manager.add<game::MainMenu>(0);
 	manager.add<game::FighterEditMenu>(1);
 	manager.add<game::FighterEdit>(2);
+	manager.add<game::FighterSelect>(3);
+	manager.add<game::Battle>(4);
+	manager.add<game::Result>(5);
+	
 
 	while (System::Update())
 	{
