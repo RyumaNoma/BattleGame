cfaf8750d59ed35785d728a490e3c9c1c0233a69
RyumaNoma <rskm.2002722@gmail.com>
2021-01-24 18:03:10 +0900

    FighterSelectシーンを画面の大きさに合わせた

--- BattleGame/FighterSelect.cpp
+++ BattleGame/FighterSelect.cpp
@@ -35,7 +35,8 @@ namespace game
 
 		// スタートボタン
 		bool isSelect = (this->selectCount > 0 && this->selectCount % 2 == 0);
-		if ((this->cursor[0].isPressed(583, 600, 783, 650) || this->cursor[1].isPressed(583, 600, 783, 650)) &&
+		if ((this->cursor[0].isPressed(Scene::Center().x - 100, 800, Scene::Center().x + 100, 870) ||
+			this->cursor[1].isPressed(Scene::Center().x - 100, 800, Scene::Center().x + 100, 870)) &&
 			isSelect)
 		{
 			for (int i = 0; i < 2; i++)
@@ -68,7 +69,8 @@ namespace game
 				cursorY = this->cursor[1].getPos().y;
 			}
 
-			if (300 <= cursorX && cursorX <= 1000 && 185 <= cursorY && cursorY <= 185 + this->allFighterData.size() * 30)
+			if (Scene::Center().x - 700 <= cursorX && cursorX <= Scene::Center().x + 700 &&
+				185 <= cursorY && cursorY < 185 + this->allFighterData.size() * 30)
 			{
 				int fighterNum = (cursorY - 185) / 30;
 
@@ -96,24 +98,24 @@ namespace game
 		drawBackButton();
 
 		// Start Game
-		drawButton(U"Start Game", Palette::Black, 583, 600, 783, 650);
+		drawButton(U"Start Game", Palette::Black, Scene::Center().x - 100, 800, Scene::Center().x + 100, 870);
 
 		// Fighterリスト
-		Line(300, 200 - 15, 1000, 200 - 15).draw(Palette::Skyblue);
+		Line(Scene::Center().x - 700, 200 - 15, Scene::Center().x + 700, 200 - 15).draw(Palette::Skyblue);
 		for (int i = 0; i < this->allFighterData.size(); i++)
 		{
 			FontAsset(U"Normal")(this->allFighterData[i].showS()).drawAt(Scene::Center().x, 200 + i * 30, Palette::Black);
-			Line(300, 200 + (i + 1) * 30 - 15, 1000, 200 + (i + 1) * 30 - 15).draw(Palette::Skyblue);
+			Line(Scene::Center().x - 700, 200 + (i + 1) * 30 - 15, Scene::Center().x + 700, 200 + (i + 1) * 30 - 15).draw(Palette::Skyblue);
 		}
 
 		// fighterプレビュー
 		if (this->selectCount > 0)
 		{
-			this->fighter[0].draw(300, 300);
+			this->fighter[0].draw(Scene::Center().x - 700, 600);
 		}
 		if (this->selectCount > 1)
 		{
-			this->fighter[1].draw(1000, 300);
+			this->fighter[1].draw(Scene::Center().x + 700, 600);
 		}
 
 		// Cursor
